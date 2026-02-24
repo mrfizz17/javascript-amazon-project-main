05c1e55136d9a8611196b2bc793abe2dab5a2050
Mustafiz Rahman <rahmanmustafizur302@gmail.com>
2026-02-25 01:13:31 +0600

    Make select quantity dropdown interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,7 +70,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -154,12 +154,14 @@ document.querySelectorAll('.js-add-to-cart-btn').forEach((button)=>{
               }
           });
 
+          let quantitySelector= document.querySelector(`.js-quantity-selector-${productId}`).value;
+
           if(matchItem){
-            matchItem.quantity++;
+            matchItem.quantity+= Number(quantitySelector);
           }else{
             cart.push({
               productId,
-              quantity:1
+              quantity:Number(quantitySelector)
             })
           }
 
