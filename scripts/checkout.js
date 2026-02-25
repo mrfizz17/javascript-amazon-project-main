657d3dbbc7bff28b7845fffad028f7cae48a1855
Mustafiz Rahman <rahmanmustafizur302@gmail.com>
2026-02-26 02:05:12 +0600

    Refactor delivery options in renderHTML to use dynamic product IDs for radio inputs

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -69,54 +69,58 @@ function renderHTML(){
               </div>
 
               <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
+                    <div class="delivery-options-title">
+                    Choose a delivery option:
+                    </div>
+                    <div class="delivery-option">
+                    <input type="radio" checked
+                        class="delivery-option-input"
+                        name="delivery-option-${checkoutItem.product.id}">
+                    <div>
+                            <div class="delivery-option-date">
+                                Tuesday, June 21
+                            </div>
+                            <div class="delivery-option-price">
+                                FREE Shipping
+                            </div>
+                    </div>
+                    </div>
+
+                    <div class="delivery-option">
+                    <input type="radio"
+                        class="delivery-option-input"
+                        name="delivery-option-${checkoutItem.product.id}">
+                    <div>
+                            <div class="delivery-option-date">
+                                Wednesday, June 15
+                            </div>
+                            <div class="delivery-option-price">
+                                $4.99 - Shipping
+                            </div>
+                    </div>
+                    </div>
+                    <div class="delivery-option">
+                    <input type="radio"
+                        class="delivery-option-input"
+                        name="delivery-option-${checkoutItem.product.id}">
+                    <div>
+                            <div class="delivery-option-date">
+                                Monday, June 13
+                            </div>
+                            <div class="delivery-option-price">
+                                $9.99 - Shipping
+                            </div>
+                    </div>
+                    </div>
+
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>`
+        </div>`
     });
 
     document.querySelector('.js-order-summary').innerHTML=pushHTML;
 
 
 }
+
+
